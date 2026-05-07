37fb32331ef32bf96bef449547c35253c41b3da4
Mefred <freds-inbox@outlook.com>
2026-05-06 20:54:43 -0700

    project restart

--- src/lib.rs
+++
@@ -1,3 +0,0 @@
-pub mod cpu;
-
-pub use cpu::CPU;
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ use FerrisNES::CPU;
 
 fn main() {
     let mut cpu = CPU::new();
-    cpu.interpret(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
+    cpu.load_and_run(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
 }
